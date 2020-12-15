6dd72130d6ff3f7fbffa264560fac2394cc483f1
satriopurbo <puka@DESKTOP-72GHHEB.localdomain>
2020-12-16 00:54:32 +0700

    login fix

--- routing/users.js
+++ routing/users.js
@@ -1,12 +1,11 @@
 const router = require('express').Router()
 const controller = require('../controller/usersController')
 const authentification = require('../middleware/authentification')
-const {authorizationAdmin} = require('../middleware/authorization')
+
 
 
 router.post('/register', controller.register)
-// router.post('/login',controller.login)
-router.post('/login',authorizationAdmin,controller.login)
+router.post('/login',controller.login)
 router.post('/update/:id',authentification,controller.update)
 router.get('/list/:id',authentification,controller.list)
 router.get('/all',authentification,controller.all)
