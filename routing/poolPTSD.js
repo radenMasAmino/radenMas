6106e0e2b7f34122780903ea2f5fca77debdb1b7
satriopurbo <puka@DESKTOP-72GHHEB.localdomain>
2020-12-20 17:06:09 +0700

    update pool -auth guest

--- routing/poolPTSD.js
+++ routing/poolPTSD.js
@@ -4,7 +4,7 @@ const authentification = require('../middleware/authentification')
 const {authorizationGuest} = require('../middleware/authorization')
 
 router.post('/register',authentification,authorizationGuest, controller.register)
-router.post('/update/:id',authentification,authorizationGuest,controller.update)
+router.post('/update/:id',authentification,controller.update)
 router.delete('/delete',authentification,authorizationGuest,controller.delete)
 router.get('/list/:id',authentification,authorizationGuest,controller.list)
 router.get('/all',authentification,authorizationGuest,controller.all)
--- routing/poolSRQ.js
+++ routing/poolSRQ.js
@@ -4,9 +4,9 @@ const authentification = require('../middleware/authentification')
 const {authorizationGuest} = require('../middleware/authorization')
 
 router.post('/register',authentification,authorizationGuest, controller.register)
-router.post('/update/:id',authentification,authorizationGuest,controller.update)
+router.post('/update/:id',authentification,controller.update)
 router.delete('/delete',authentification,authorizationGuest,controller.delete)
-router.get('/list/:id',authentification,authorizationGuest,controller.list)
+router.get('/list/:id',authentification,controller.list)
 router.get('/all',authentification,authorizationGuest,controller.all)
 router.post('/screening',authentification,authorizationGuest,controller.screening)
  
