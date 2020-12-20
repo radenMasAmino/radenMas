cdc69766d29a1bff55779447f732b1a5e1e87325
satriopurbo <puka@DESKTOP-72GHHEB.localdomain>
2020-12-21 00:21:39 +0700

    authguest list delete

--- routing/poolKecemasan.js
+++ routing/poolKecemasan.js
@@ -6,7 +6,7 @@ const {authorizationGuest} = require('../middleware/authorization')
 router.post('/register',authentification,authorizationGuest, controller.register)
 router.post('/update/:id',authentification,controller.update)
 router.delete('/delete',authentification,authorizationGuest,controller.delete)
-router.get('/list/:id',authentification,authorizationGuest,controller.list)
+router.get('/list/:id',authentification,controller.list)
 router.get('/all',authentification,authorizationGuest,controller.all)
 router.post('/screening',authentification,authorizationGuest,controller.screening)
  
